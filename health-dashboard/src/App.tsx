8726d0422dedf2f7eb0959c1f7723bfce1f577ec
Justin Ecarma <just.ecarma@gmail.com>
2025-10-21 23:48:41 -0400

    Visualizations added and completed

--- health-dashboard/src/App.tsx
+++ health-dashboard/src/App.tsx
@@ -11,39 +11,36 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>COVID-19 Health Dashboard</h1>
-      
+
       {/* COVID-19 Risk Predictor */}
       <Predictor />
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
       
       <Visualization 
         imagePath="/bp-analysis.png" 
         title="Blood Pressure Analysis"
         description="Distribution of COVID-19 cases across different blood pressure ranges. The data shows relatively consistent positive rates across all ranges."
       />
-      
+
+      <Visualization
+        imagePath="/age-analysis.png"
+        title="Age Analysis"
+        description="Distribution of COVID-19 survey respondents. The data shows a relatively even distribution of respondents"
+      />
+
+      <Visualization
+        imagePath="/cmbd-analysis.png"
+        title="Blood Pressure Analysis"
+        description="Distribution of COVID-19 cases across different heart rate ranges. The data shows relatively consistent positive rates across all ranges."
+      />
+
+      <Visualization
+        imagePath="/hr-analysis.png"
+        title="Blood Pressure Analysis"
+        description="Distribution of COVID-19 cases across different comorbidity ranges. The data shows relatively consistent positive rates across all ranges."
+      />
+
       <MetadataTable csvPath="/metadata.csv" title="Dataset Statistics" />
-      
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
